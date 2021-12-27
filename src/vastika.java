be8153fa0ecce125e3b2ce037cc152ddeaf6bdc2
Sudip Adhikari <sadhikaris2016@gmail.com>
2021-12-27 02:53:50 -0600

    initial commit

---
+++ src/vastika.java
@@ -0,0 +1,12 @@
+public class vastika {
+    public static void main(String[] args){
+        String greeting="day1. Welcome to the core java";
+        System.out.println(greeting);
+        for(int i=0; i < greeting.length(); i ++) {
+        System.out.println("*");
+
+    }
+        System.out.println();
+}
+}
+
